51becd261f99364776c42f2f67a2c7dda615d84d
Caroline <carol-html@hotmail.com>
2025-10-25 11:59:30 -0300

    Refactor: Altera o status do botão clicado e imagem correspondente

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 /*
  * Ao clicar no botão do jogo, deverá verificar se está alugado, trocar o texto do botão e mudar o visual do    
  * elemento.
+ * 
+ * 1- se o botão for igual a devolver, então a imagem deve mudar a aparência para acesa e o texto Devolver deve mudar para Alugar e tbm sua aparência,
+ * se não (o texto do botão for igual a Alugar - só tem essa possibilidade) a imagem deve mudar a aparência para um pouco mais apagada e o texto do botão Alugar deve mudar para Devolver, assim como sua aparência
  */ 
 
-// altera o status do botão clicado para alugar ou devolver
-
+/*
 function alterarStatus(jogo){
     let botao = document.getElementById(`btn-alugar-${jogo === 1 ? 'monopoly' : jogo === 2 ? 'ticket-to-ride' : 'takenoko'}`)
     let txtBotao = botao.innerText;
@@ -17,7 +19,26 @@ function alterarStatus(jogo){
         botao.classList.add('dashboard__item__button--return');
     }   
 }
+*/
 
+function alterarStatus(id){
+    let jogoClicado = document.getElementById(`game-${id}`);
+    let imagem = jogoClicado.querySelector('.dashboard__item__img');
+    let botao = jogoClicado.querySelector('.dashboard__item__button');
+    let jogo = jogoClicado.querySelector('.dashboard__item__name');
+
+    // Altera o status dos elementos correspondentes ao botão clicado
+
+    if(botao.textContent === 'Devolver'){
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';  
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';  
+    }
+}
 
 
 
