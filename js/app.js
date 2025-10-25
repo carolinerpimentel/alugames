0ed7710d16f75179bb7a729c94fe7633e9d9f36b
Caroline <carol-html@hotmail.com>
2025-10-25 13:46:30 -0300

    feat: confirma a devolução do jogo

--- js/app.js
+++ js/app.js
@@ -7,19 +7,25 @@ function alterarStatus(id){
     let jogoClicado = document.getElementById(`game-${id}`);
     let imagem = jogoClicado.querySelector('.dashboard__item__img');
     let botao = jogoClicado.querySelector('.dashboard__item__button');  
+    let nomeJogo = jogoClicado.querySelector('.dashboard__item__name');
 
     // Altera o status dos elementos correspondentes ao botão clicado
 
     if(imagem.classList.contains('dashboard__item__img--rented')){
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';  
+        // confirma a devolução do jogo
+        if(confirm(`Tem certeza que deseja devolver ${nomeJogo.textContent}?`)){
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';  
+        } 
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';  
     }
 }
+
+
 
 
 
