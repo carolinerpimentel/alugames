b02ec62993142630e234a99db98654302d67c613
Caroline <carol-html@hotmail.com>
2025-10-25 12:06:06 -0300

    refactor: remove comentários temporários de desenvolvimento

--- js/app.js
+++ js/app.js
@@ -1,24 +1,6 @@
 /*
  * Ao clicar no botão do jogo, deverá verificar se está alugado, trocar o texto do botão e mudar o visual do    
  * elemento.
- * 
- * 1- se o botão for igual a devolver, então a imagem deve mudar a aparência para acesa e o texto Devolver deve mudar para Alugar e tbm sua aparência,
- * se não (o texto do botão for igual a Alugar - só tem essa possibilidade) a imagem deve mudar a aparência para um pouco mais apagada e o texto do botão Alugar deve mudar para Devolver, assim como sua aparência
- */ 
-
-/*
-function alterarStatus(jogo){
-    let botao = document.getElementById(`btn-alugar-${jogo === 1 ? 'monopoly' : jogo === 2 ? 'ticket-to-ride' : 'takenoko'}`)
-    let txtBotao = botao.innerText;
-
-    if(txtBotao === 'Devolver'){
-        botao.innerHTML = 'Alugar';  
-        botao.classList.remove('dashboard__item__button--return');      
-    } else {
-        botao.innerHTML = 'Devolver';
-        botao.classList.add('dashboard__item__button--return');
-    }   
-}
 */
 
 function alterarStatus(id){
